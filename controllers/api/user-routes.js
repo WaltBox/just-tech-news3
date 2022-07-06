0c976990861a8ce7809f8eb1eb52861fa1c64411
Walt Boxwell <waltboxwell@gmail.com>
2022-07-05 19:40:51 -0500

    problem with login form

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -62,36 +62,46 @@ router.post('/',(req,res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
+      })
 });
 
-router.post('/login',(req, res) => {
-    User.findOne({
-        where: {
+    router.post('/login', (req, res) => {
+        User.findOne({
+          where: {
             email: req.body.email
-        }
-    }).then(dbUserData => {
-        if(!dbUserData) {
-            res.status(400).json({message: 'No user with that email address'});
+          }
+        }).then(dbUserData => {
+          if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
             return;
-        }
-
-        console.log(dbUserData)
-        // verify user
-        const validPassword = dbUserData.checkPassword(req.body.password);
-        console.log(validPassword)
-        if (!validPassword){
-            res.status(400).json({ message: 'Incorrect password!'});
+          }
+      
+          const validPassword = dbUserData.checkPassword(req.body.password);
+      
+          if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
             return;
-        }
-
-        res.json({ user: dbUserData, message: 'You are logged in!'});
-    });
-});
+          }
+      
+          req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+      
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+            console.log(req.session);
+          });
+        });
+      })
 
 // PUT /api/users/1
 router.put('/:id',(req, res)=> {
--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie:{},
+    resave:false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+app.use(session(sess));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
